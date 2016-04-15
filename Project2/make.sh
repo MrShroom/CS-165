b97006979da96a25e0bf96db84869355b256f4f3
Ian Schweer <schweer@adobe.com>
2016-04-15 16:20:56 -0700

    Added project 2

---
+++ Project2/make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if test "$1" == "" ;then
+	echo "Incorrect number of arguments. Please provide a file name">&2 
+	exit 1
+fi
+
+file=$1
+if [ -f $file ]; then
+	if [ -h "mysub.c" ]; then
+		rm mysub.c
+	fi
+	ln -s $file mysub.c
+	ls
+	make
+else
+	echo "File does not exist"
+fi
+# create symbolic link for the compiling file
