82be5b0518c917e7bcef81c05f7ff790d63d8c10
Shaun McThomas <Shaunrm23@gmail.com>
2016-05-30 23:37:32 -0700

    Updating encoding

--- Project3/source/encoder.cpp
+++ Project3/source/encoder.cpp
@@ -21,6 +21,12 @@ byte *string_reference_tuplet::encode(const Options& opt, std::vector<encoded_tu
 		bits.set(j, (offset & i) >> j);
 	}
 	res.push_back(encoded_tuplet(bits, opt.getL() + opt.getN()));
+    if (opt.getDebug()) {
+        std::cerr << "string reference tuplet <" << ++len << ", " << offset << ">";
+        std::cerr << " in binary is (" << opt.getL() + opt.getN() << ") " << bits.to_string().substr(MAX_BIT_SET - (opt.getL() + opt.getN()), opt.getL());
+        std::cerr << "|" << bits.to_string().substr(opt.getL(), opt.getN());
+        std::cerr << std::endl;
+    }
 	return NULL;
 }
 
@@ -29,12 +35,19 @@ byte *character_tuplet::encode(const Options& opt, std::vector<encoded_tuplet> &
 
 	// automatically skip S bits since we never set the leading S bits
 	for (int j=0, i = 1; j < opt.getS(); j++,i = i<<1) {
-		bits.set(j + opt.getL(), (strlen & i) >> j);
+		bits.set(j + opt.getL() - 1, (strlen & i) >> j);
 	}
 
 	for (int i = 0; i < strlen; i++) {
-		bits.set(i, c[i] == '1');
+		bits.set(i, c[strlen-i-1] == '1');
 	}
 	t.push_back(encoded_tuplet(bits, opt.getS() + opt.getL() + strlen));
+    if (opt.getDebug()) {
+        std::cerr << bits.to_string() << std::endl;
+        std::cerr << "character tuplet <0, " << strlen << ", " << code << ">";
+        std::cerr << " in binary is (" << opt.getL() + opt.getS() + strlen << ") ";
+        std::cerr << bits.to_string().substr(MAX_BIT_SET - (opt.getL() + opt.getS() + strlen));
+        std::cerr << std::endl;
+    }
 }
 
